c611171df6317619c9b4ba5e0fe05f2df048c9cc
Gunnar Westerling <gunnarwesterling@gmail.com>
2023-06-13 14:41:53 +0200

    Implement deadman's switch for safety

--- source.ino
+++ source.ino
@@ -1,9 +1,18 @@
 /*
- * 01.09.2020
+ * 04.09.2020
  * Der ESP32 ist in der Lage zuverlässig den Motor zu steuern und Messdaten zu lesen. 
  * Das Display zeigt die aktuelle Geschwindigkeit an. Außerdem gibt das Display eine Warnung bei Übertemperatur aus. 
+ * Danach wurde ein Sicherheitsmechanismus implementiert, bei dem ein Schalter geschlossen bleiben muss, anderenfalls wird der Motor gebremst. 
  */
- 
+
+ /*
+  * Pin Belegung:
+  * Serial (VESC 1): RX = RX, TX = TX
+  * Serial2 (VESC 2): RX = 16, TX = 17
+  * I2C: SDA = 21, SCL = 22, GND = GND, VCC = 5V/3.3V
+  * Totmannschalter = 18
+  */
+
 //Include libraries for VESC
 #include <HardwareSerial.h>
 #include <ESP8266VESC.h>
@@ -16,6 +25,15 @@
 //Define display size
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+//Define GPIO pin for button
+#define BUTTON 18
+
+//Global variables
+boolean openswitch;
+boolean vescunavailable;
+boolean overheating1;
+boolean overheating2;
 
 //Create required class instances
 ESP8266VESC esp8266VESC1 = ESP8266VESC(Serial2); //VESC 1
@@ -49,61 +67,94 @@ boolean readVESC() {
   }
 }
 
+//Read all sensors function
+void readSensors() {
+  //Read safety switch 
+  if(digitalRead(BUTTON)){
+    openswitch = true;
+  }
+  else {
+    openswitch = false;
+  }
 
-//if overheating show warning, otherwise show velocity
+  //Read VESC sensors
+  vescunavailable = !readVESC();
+
+  //Check for overheating after reading VESC sensors
+  overheating1 = overtemperature(vescValues1);
+  overheating2 = overtemperature(vescValues2);
+}
+
+
+//Control motor function
+void drive(int rpm) {
+  if (overtemperature(vescValues1) || overtemperature(vescValues2) || vescunavailable) {
+    esp8266VESC1.releaseEngine();
+    esp8266VESC2.releaseEngine();
+  }
+  else {
+    if (openswitch) {
+      //Brake
+      esp8266VESC1.fullBreaking();
+      esp8266VESC2.fullBreaking();
+    }
+    else {
+      esp8266VESC1.setRPM(rpm);
+      esp8266VESC2.setRPM(rpm);
+    }
+  }
+}
+
+
+//Write on display function
 void showOnDisplay () {
   //clear display from previous values
   display.clearDisplay();
 
   //set cursor to position to keep image stable
-  display.setCursor(0, 10);
+  display.setCursor(0, 0);
 
-  int velocity1;
-  int velocity2;
-  boolean overheating1 = overtemperature(vescValues1);;
-  boolean overheating2 = overtemperature(vescValues2);
-
-  if (overheating1 && overheating2) {
-    display.setTextSize(2);
-    display.println("WARNING: Both VESCs overheat! Driving blocked for safety");
-  }
-  else if (overheating1) {
-    display.setTextSize(2);
-	  display.println("WARNING: VESC 1 overheats! Driving blocked for safety");
-  }
-  else if (overheating2) {
-    display.setTextSize(2);
-	  display.println("WARNING: VESC 2 overheats! Driving blocked for safety");
+  //Show message depending on current variable states
+  if (vescunavailable) {
+    display.setTextSize(1);
+    display.println("ERROR: Connection    failure to one or    both Motorcontrollers");
   }
   else {
-    //display average velocity on display
-    //string rpm1 = string.toString(vescValues1.rpm);
-    velocity1 = (int)((vescValues1.rpm * 254) / 60000);
-	  velocity2 = (int)((vescValues2.rpm * 254) / 60000);
-	  
-	  int velocity = (velocity1 + velocity2) / 2;
+    if (openswitch) {
+      //Show message
+      display.setTextSize(1);
+      display.println("Please close security mechanism");    
+    }
+    else {
+      if (overheating1 && overheating2) {
+        display.setTextSize(2);
+        display.println("WARNING: Both VESCs overheat! Driving blocked for safety");
+      }
+      else if (overheating1) {
+        display.setTextSize(2);
+        display.println("WARNING: VESC 1 overheats! Driving blocked for safety");
+      }
+      else if (overheating2) {
+        display.setTextSize(2);
+        display.println("WARNING: VESC 2 overheats! Driving blocked for safety");
+      }
+      else {
+        //display average velocity on display
+        //string rpm1 = string.toString(vescValues1.rpm);
+        int velocity1 = (int)((vescValues1.rpm * 254) / 60000);
+        int velocity2 = (int)((vescValues2.rpm * 254) / 60000);
+        int velocity = (velocity1 + velocity2) / 2;
     
-    display.setTextSize(2);
-    display.print(vescValues1.rpm);
-    display.print(" km/h");
-    display.println();
+        display.setTextSize(2);
+        display.print(vescValues1.rpm);
+        display.print(" km/h");
+        display.println();
+      }    
+    }
   }
 
-  //show on display
+  //show message on display
   display.display(); 
-}
-
-
-//sets the rpm value to the given parameter, except if overtemperature is detected in which case the motor goes to idle
-void drive(int rpm) {
-  if (overtemperature(vescValues1) && overtemperature(vescValues2)) {
-    esp8266VESC1.releaseEngine();
-    esp8266VESC2.releaseEngine();
-  }
-  else {
-    esp8266VESC1.setRPM(rpm);
-    esp8266VESC2.setRPM(rpm);
-  }
 }
 
 
@@ -116,6 +167,10 @@ void setup() {
   //Setup serial connection to VESC 1
   Serial2.begin(115200);
   delay(50);
+
+  //Setup GPIO pin with button for interrupt
+  pinMode(BUTTON, INPUT_PULLUP);
+  //attachInterrupt(BUTTON, ISR, CHANGE);  
 
   //Initialize display
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
@@ -134,22 +189,14 @@ void setup() {
 
 //Program routine
 void loop() {  
-  //Read sensors, if sensor values couldn't be set idle and display error message
-  while(!readVESC()) {
-	  esp8266VESC1.releaseEngine();
-	  esp8266VESC2.releaseEngine();
-   
-    display.setTextSize(1);
-    display.setCursor(0, 0);
-	  display.println("ERROR: Connection    failure to one or    both Motorcontrollers");
-	  display.display();
-  }
-  
-  //Show velocity on display, give warning if overheating
-  showOnDisplay();
-  
-  //Set given rpm value, if motor is not overheating
+  //Read all sensors
+  readSensors();
+
+  //Control motor
   drive(0);
+  
+  //Write on display
+  showOnDisplay();
 }
 
 /*
